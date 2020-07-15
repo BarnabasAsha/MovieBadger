07919b2042bd455667158f5cfd544894c8148528
Barnabas Asha <barnabee58@gmail.com>
2020-07-15 17:29:24 +0100

    Initialized Movie Badger

---
+++ includes/mailer.php
@@ -0,0 +1,45 @@
+<?php
+$email = $_GET['email'];
+
+if (isset($email)) {
+    # code...
+
+    $dbServerName = "localhost";
+            $dbUserName = "root";
+            $dbPassword = "TechBee15";
+            $dbName = "moviebadger";
+
+            $conn = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
+            $cmd = "INSERT INTO emaillist (email) value('$email');";
+            mysqli_query($conn, $cmd);
+
+    if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+        # code...
+        echo 'Please Enter a valid email';
+    } else {
+        $from = 'MovieBadger';
+        $subject = 'Notification Confirmation';
+        $body = '
+        <div class="mail-container" style="background: linear-gradient(81.7deg, #1A2632 35.21%, #F0324F 116.87%); color: white; padding: 30px; text-align: center; width: 280px; height: 200px; margin: auto; font-family: sans-serif;">
+        <h3 style="font-size: 14px; font-weight: bold; margin-bottom: 20px;">Movie Badger</h3>
+        <h1 style="font-size: 30px; font-weight: bold;">Thank you!</h1>
+        <p style="font-size: 16px;">We would contact you when we launch</p>
+    </div>
+        ';
+        $headers = "MIME-Version: 1.0" ."\r\n";
+        $headers .= "Content-Type:text/html;charset=UTF-8" ."\r\n";
+        $headers .= "From: " .$from ."\r\n";
+
+        if (mail($email, $subject, $body, $headers)) {
+            # code...
+            echo 'Thank you, You will be notified';
+        } else {
+            echo 'Please Try Again';
+        }
+
+            
+    }
+}
+
+
+?>
---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie Badger</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/animate.css">
+    <link rel="stylesheet" href="./css/bootstrap/bootstrap.css">
+</head>
+<body>   
+    <section>
+        <div class="header">
+            <div class="wrapper">
+            <div class="logo">moviebadger</div>
+            <div class="showcase">
+                <div class="showcase-text animated bounceInDown">
+                    <h1>Find the title to any movie with the world's first movie finder.</h1>
+                    <p>Moviebadger helps you remember movies.  Find and save movies to your favorite list, get movies info and even download links
+                    </p>
+                    
+                    <form method="POST" id="subscribe">
+                    <h6 class="mb-3">BE THE FIRST TO GET ACCESS WHEN THE APP GOES LIVE</h6>
+                        <div class="alert alertOne"></div>
+                        <div class="form-pair">
+                            <input type="email" name="email" id="email" placeholder="Enter your email address">
+                            <button type="submit"><img src="./images/Vector.svg" alt=""></button>
+                            </div>
+                    </form>
+                </div>
+                
+                    <img class="bg-1 animated bounceInUp" src="./images/screen.png" alt="">
+                    <img class="bg-2 animated bounceInUp" src="./images/screen.png" alt="">
+            
+            </div>
+        </div>
+            </div>
+    </section>
+    <div class="main">
+    
+    </div> 
+    <section>
+        <div class="container">
+            <div class="content d-flex flex-row justify-content-center align-items-center wow fadeInLeft">
+                <div class="img mr-5">
+                    <img src="./images/screen.png" alt="">
+                </div>
+                <div class="text ml-5">
+                    <h2>Find the title to any movie with the world’s first movie finder.</h2>
+                    <p>Moviebadger helps you remember movies. Find and save movies to your favourite list, get movie info and even download links!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <div class="content d-flex flex-row justify-content-center align-items-center flex-row-reverse wow fadeInRight">
+                <div class="img ml-5">
+                    <img src="./images/screen.png" alt="">
+                </div>
+                <div class="text mr-5">
+                    <h2>Find the title to any movie with the world’s first movie finder.</h2>
+                    <p>Moviebadger helps you remember movies. Find and save movies to your favourite list, get movie info and even download links!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="content d-flex flex-row justify-content-center align-items-center wow fadeInLeft">
+        <div class="img mr-5">
+                    <img src="./images/screen.png" alt="">
+                </div>
+                <div class="text ml-5">
+                    <h2 style="color: #fff;">Find the title to any movie with the world’s first movie finder.</h2>
+                    <p style="color: #fff;">Moviebadger helps you remember movies. Find and save movies to your favourite list, get movie info and even download links!
+                    </p>
+                </div>
+            </div>
+    </section>
+
+    <div class="rotate">
+
+    </div>
+
+    <section>
+        <div class="container">
+            <div class="content d-flex flex-row justify-content-center align-items-center flex-row-reverse mt-5 wow fadeInRight">
+                <div class="img ml-5">
+                    <img src="./images/screen.png" alt="">
+                </div>
+                <div class="text mr-5">
+                    <h2>Find the title to any movie with the world’s first movie finder.</h2>
+                    <p>Moviebadger helps you remember movies. Find and save movies to your favourite list, get movie info and even download links!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+           <div class="footer">
+           <div class="container d-flex flex-column justify-content-center align-items-center">
+           <h6 class="text-center mb-5">BE THE FIRST TO GET ACCESS WHEN THE APP GOES LIVE</h6>
+            <form method="POST" id="subscribe2">
+                        <div class="alert alertTwo"></div>
+                            <div class="form-pair">
+                            <input type="email" name="email" id="email2" placeholder="Enter your email address">
+                            <button type="submit"><img src="./images/Vector.svg" alt=""></button>
+                            </div>
+    </form>
+            </div>
+           </div>
+    </footer>
+    
+    
+
+
+
+</body>
+<script src="./includes/wow.js"></script>
+    <script>
+        new WOW().init()
+    </script>
+    <script src="./includes/script.js"></script>
+</html>
