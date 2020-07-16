0f56138bc663eb4d8e7bdd56d9d0f8b92dd32f44
Barnabas Asha <barnabee58@gmail.com>
2020-07-16 07:32:38 +0100

    Email sending fixes

--- includes/mailer.php
+++ includes/mailer.php
@@ -17,20 +17,21 @@ if (isset($email)) {
         # code...
         echo 'Please Enter a valid email';
     } else {
-        $from = 'MovieBadger';
-        $subject = 'Notification Confirmation';
+        $from = "MovieBadger";
+        $subject = "Notification Confirmation";
         $body = '
-        <div class="mail-container" style="background: linear-gradient(81.7deg, #1A2632 35.21%, #F0324F 116.87%); color: white; padding: 30px; text-align: center; width: 280px; height: 200px; margin: auto; font-family: sans-serif;">
+        <div class="container" style="background: linear-gradient(81.7deg, #1A2632 35.21%, #F0324F 116.87%); color: white; padding: 30px; text-align: center; width: 280px; height: 200px; margin: auto; font-family: sans-serif;">
         <h3 style="font-size: 14px; font-weight: bold; margin-bottom: 20px;">Movie Badger</h3>
         <h1 style="font-size: 30px; font-weight: bold;">Thank you!</h1>
         <p style="font-size: 16px;">We would contact you when we launch</p>
     </div>
         ';
         $headers = "MIME-Version: 1.0" ."\r\n";
-        $headers .= "Content-Type:text/html;charset=UTF-8" ."\r\n";
+        $headers .= "Content-Type:text/html" ."\r\n";
+        $headers .= "charset=UTF-8" ."\r\n";
         $headers .= "From: " .$from ."\r\n";
-
-        if (mail($email, $subject, $body, $headers)) {
+        $send = mail($email,$subject,$body,$headers);
+        if ($send == true) {
             # code...
             echo 'Thank you, You will be notified';
         } else {
